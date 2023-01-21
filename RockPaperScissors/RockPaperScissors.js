bea9f91f7c42c49ea7a99019b55c1fea2601b84c
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-21 21:21:49 +0100

    RockPaperScissors

---
+++ RockPaperScissors/RockPaperScissors.js
@@ -0,0 +1,5 @@
+console.log('hi');
+console.log("Make your choice : Rock - Paper, or Scissors ! Choose wisely.");
+function getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+};
