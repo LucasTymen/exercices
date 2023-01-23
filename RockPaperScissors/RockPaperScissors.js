64ef2643b0c630cfc31316f1c988d6bb5ce5fad2
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-23 13:08:38 +0100

    computerChoice Fixed

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -1,5 +1,6 @@
 console.log("hi");
 console.log("Make your choice : Rock - Paper, or Scissors ! Choose wisely.");
+
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
   if (
@@ -15,7 +16,18 @@ const getUserChoice = (userInput) => {
 console.log(getUserChoice("ROCK"));
 
 const getComputerChoice = () => {
-  Math.floor(Math.random() * 3);
-  return getComputerChoice;
-}
-console.log(getComputerChoice)
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissor";
+    default:
+      return "nawak";
+      break;
+  }
+};
+
+console.log(getComputerChoice());
