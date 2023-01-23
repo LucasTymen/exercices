f0a24af6c4ddf394f2ba384c0df40a142fe01cb4
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-23 11:52:43 +0100

    RockPaperScissors_computerChoice

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -1,5 +1,21 @@
-console.log('hi');
+console.log("hi");
 console.log("Make your choice : Rock - Paper, or Scissors ! Choose wisely.");
-function getUserChoice = (userInput) => {
+const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissor"
+  ) {
+    return userInput;
+  } else {
+    console.log("didn't understood");
+  }
 };
+console.log(getUserChoice("ROCK"));
+
+const getComputerChoice = () => {
+  Math.floor(Math.random() * 3);
+  return getComputerChoice;
+}
+console.log(getComputerChoice)
